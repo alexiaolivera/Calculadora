96a25422b4fa60b7238358be4f3d7cc9f911bb3c
alexiaolivera <alexiaolivera43@gmail.com>
2021-12-03 19:44:19 -0300

    actualización

--- script.js
+++ script.js
@@ -1,31 +1,99 @@
-function tomarDatos(e){
+ var bsuma = document.getElementById("bsuma");
 
+    bsuma.addEventListener("click", function(e){
+        e.preventDefault();    
+        resolver("suma");
+    });
+
+var bresta = document.getElementById("bresta");
+
+    bresta.addEventListener("click", function(e){
+        e.preventDefault();    
+        resolver("resta");
+    });
+
+var bdivision = document.getElementById("bdivision");
+
+    bdivision.addEventListener("click", function(e){
+        e.preventDefault();    
+        resolver("division");
+    });
+
+var bmultiplicacion = document.getElementById("bmultiplicacion");
+
+    bmultiplicacion.addEventListener("click", function(e){
+        e.preventDefault();    
+        resolver("multiplicacion");
+    });
+
+var bpotencia = document.getElementById("bpotencia");
+
+    bpotencia.addEventListener("click", function(e){
+        e.preventDefault();    
+        resolver("potencia");
+    });
+
+var braiz = document.getElementById("braiz");
+
+    braiz.addEventListener("click", function(e){
+        e.preventDefault();    
+        resolver("raiz");
+    });
+
+var historial = "";
+var contador = 0;
+var btnBorrarHistorial = document.getElementById("btnBorrarHistorial");
+btnBorrarHistorial.addEventListener("click", function(e){
     e.preventDefault();
-    var uno = document.getElementById("uno").value;
-    var dos = document.getElementById("dos").value;
+    contador = 0;
+    historial = "";
+    document.getElementById("historial").innerHTML = historial = "";;
+    document.getElementById("resultado").textContent = "";
+});
 
-    var operador = document.getElementById("operador").value;
-    if(operador == "suma"){
-        var resultado = parseInt(uno) + parseInt(dos);
+function resolver(operacion){
+    var uno = parseFloat(document.getElementById("uno").value);
+    var dos = parseFloat(document.getElementById("dos").value);
+    var resultado = 0;
+
+    switch(operacion){
+        case "suma":
+            resultado = uno + dos;
+            contador++;
+            historial = historial+ contador+") " + uno + " + " + dos + " = " + resultado + "<br>";
+            break;
+
+        case "resta":
+            contador++;
+            resultado = uno - dos;
+            historial = historial + contador+") " + uno + " - " + dos + " = " + resultado + "<br>";
+            break;
+
+        case "multiplicacion":
+            contador++;
+            resultado = uno * dos;
+            historial = historial + contador+") " + uno + " * " + dos + " = " + resultado + "<br>";
+            break;
+
+        case "division":
+            contador++;
+            resultado = uno / dos;
+            historial = historial + contador+") " + uno + " / " + dos + " = " + resultado + "<br>";
+            break;
+
+        case "potencia":
+            contador++;
+            resultado = Math.pow(uno, dos);
+            historial = historial + contador+") " + uno + " ^ " + dos + " = " + resultado + "<br>";
+            break;
+
+        case "raiz":
+            contador++;
+            resultado = Math.pow(uno, 1/dos);
+            historial = historial + contador+") " + dos +"√" + uno + " = " + resultado + "<br>";
+            break;
     }
-    if(operador == "resta"){
-        var resultado = parseInt(uno) - parseInt(dos);
-    }
-    if(operador == "multiplicacion"){
-        var resultado = parseInt(uno) * parseInt(dos);
-    }
-    if(operador == "division"){
-        var resultado = parseInt(uno) / parseInt(dos);
-    }
-    if(operador == "potencia"){
-        var resultado =Math.pow(parseInt(uno),parseInt(dos));
-    }
-    if(operador == "raiz"){
-        var resultado =Math.sqrt(parseInt(uno));
-    }
-    document.querySelector("h2").textContent= resultado;
+    console.log(historial);
+    document.getElementById("historial").innerHTML = historial;
+    document.getElementById("resultado").textContent = resultado;
 }
-
-var formulario = document.querySelector("#formulario");
-formulario.addEventListener("submit",tomarDatos);
-                                    
